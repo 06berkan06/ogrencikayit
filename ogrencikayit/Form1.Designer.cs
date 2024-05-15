9203a69e9e145f319b166f2a489df371a45e8e46
User <User@LAB2-13>
2024-05-15 11:06:04 +0300

    Add project files.

---
+++ ogrencikayit/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace ogrencikayit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridadam = new System.Windows.Forms.DataGridView();
+            this.txtno = new System.Windows.Forms.TextBox();
+            this.btngüncelle = new System.Windows.Forms.Button();
+            this.btnsil = new System.Windows.Forms.Button();
+            this.btnekle = new System.Windows.Forms.Button();
+            this.txtarama = new System.Windows.Forms.TextBox();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.txtsoyad = new System.Windows.Forms.TextBox();
+            this.txttel = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbltel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridadam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridadam
+            // 
+            this.gridadam.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridadam.Location = new System.Drawing.Point(234, 114);
+            this.gridadam.Name = "gridadam";
+            this.gridadam.Size = new System.Drawing.Size(370, 201);
+            this.gridadam.TabIndex = 0;
+            this.gridadam.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridadam_CellContentClick);
+            // 
+            // txtno
+            // 
+            this.txtno.Location = new System.Drawing.Point(367, 348);
+            this.txtno.Name = "txtno";
+            this.txtno.Size = new System.Drawing.Size(100, 20);
+            this.txtno.TabIndex = 1;
+            // 
+            // btngüncelle
+            // 
+            this.btngüncelle.Location = new System.Drawing.Point(512, 401);
+            this.btngüncelle.Name = "btngüncelle";
+            this.btngüncelle.Size = new System.Drawing.Size(75, 23);
+            this.btngüncelle.TabIndex = 2;
+            this.btngüncelle.Text = "güncelle";
+            this.btngüncelle.UseVisualStyleBackColor = true;
+            this.btngüncelle.Click += new System.EventHandler(this.btngüncelle_Click);
+            // 
+            // btnsil
+            // 
+            this.btnsil.Location = new System.Drawing.Point(512, 435);
+            this.btnsil.Name = "btnsil";
+            this.btnsil.Size = new System.Drawing.Size(75, 23);
+            this.btnsil.TabIndex = 2;
+            this.btnsil.Text = "sil";
+            this.btnsil.UseVisualStyleBackColor = true;
+            this.btnsil.Click += new System.EventHandler(this.btnsil_Click);
+            // 
+            // btnekle
+            // 
+            this.btnekle.Location = new System.Drawing.Point(512, 372);
+            this.btnekle.Name = "btnekle";
+            this.btnekle.Size = new System.Drawing.Size(75, 23);
+            this.btnekle.TabIndex = 3;
+            this.btnekle.Text = "ekle";
+            this.btnekle.UseVisualStyleBackColor = true;
+            this.btnekle.Click += new System.EventHandler(this.btnekle_Click);
+            // 
+            // txtarama
+            // 
+            this.txtarama.Location = new System.Drawing.Point(376, 71);
+            this.txtarama.Name = "txtarama";
+            this.txtarama.Size = new System.Drawing.Size(100, 20);
+            this.txtarama.TabIndex = 4;
+            this.txtarama.TextChanged += new System.EventHandler(this.txtarama_TextChanged);
+            // 
+            // txtad
+            // 
+            this.txtad.Location = new System.Drawing.Point(367, 375);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(100, 20);
+            this.txtad.TabIndex = 5;
+            // 
+            // txtsoyad
+            // 
+            this.txtsoyad.Location = new System.Drawing.Point(367, 404);
+            this.txtsoyad.Name = "txtsoyad";
+            this.txtsoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtsoyad.TabIndex = 6;
+            // 
+            // txttel
+            // 
+            this.txttel.Location = new System.Drawing.Point(367, 445);
+            this.txttel.Name = "txttel";
+            this.txttel.Size = new System.Drawing.Size(100, 20);
+            this.txttel.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(293, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ögrenci arama";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 348);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "no";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(293, 382);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "ad";
+            // 
+            // lbltel
+            // 
+            this.lbltel.AutoSize = true;
+            this.lbltel.Location = new System.Drawing.Point(293, 445);
+            this.lbltel.Name = "lbltel";
+            this.lbltel.Size = new System.Drawing.Size(39, 13);
+            this.lbltel.TabIndex = 12;
+            this.lbltel.Text = "telefon";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(293, 411);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "soyad";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 498);
+            this.Controls.Add(this.lbltel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txttel);
+            this.Controls.Add(this.txtsoyad);
+            this.Controls.Add(this.txtad);
+            this.Controls.Add(this.txtarama);
+            this.Controls.Add(this.btnekle);
+            this.Controls.Add(this.btnsil);
+            this.Controls.Add(this.btngüncelle);
+            this.Controls.Add(this.txtno);
+            this.Controls.Add(this.gridadam);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridadam)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridadam;
+        private System.Windows.Forms.TextBox txtno;
+        private System.Windows.Forms.Button btngüncelle;
+        private System.Windows.Forms.Button btnsil;
+        private System.Windows.Forms.Button btnekle;
+        private System.Windows.Forms.TextBox txtarama;
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.TextBox txtsoyad;
+        private System.Windows.Forms.TextBox txttel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbltel;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ogrencikayit/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Xml.Linq;
+
+namespace ogrencikayit
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection connection;
+        DataTable table;
+        MySqlDataAdapter adapter;
+        MySqlCommand komut;
+
+        string baglanticumlesi = "Server=localhost;Database=ögrenciii;Uid=root;Pwd=1234;";
+
+        public MySqlConnection baglan()
+        {
+
+           
+            
+            MySqlConnection.ClearPool(connection);
+            return connection;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Listele();
+        }
+
+        private void Listele()
+        {
+            connection = new MySqlConnection(baglanticumlesi);
+            
+           adapter = new MySqlDataAdapter("SELECT * FROM ögrenciii.ogrenci", baglanticumlesi);
+           table = new DataTable();
+          connection.Open();
+            adapter.Fill(table);
+            gridadam.DataSource = table;
+            connection.Close();
+
+
+        }
+        public void temizle()
+        {
+            txtad.Clear();
+            txtno.Clear();
+            txtsoyad.Clear();
+            txttel.Clear();
+        }
+        string eklekodu;
+        private void btnekle_Click(object sender, EventArgs e)
+        {
+            komut=new MySqlCommand();
+            connection.Open();
+            
+            komut.Connection = connection;
+            komut.CommandText= "INSERT INTO ogrenci(no,ad,soyad,tel)values ('" + txtno.Text + "','" + txtad.Text + "','" + txtsoyad.Text + "','" + txttel.Text + "')"; ;
+            //eklekodu = 
+            
+            //komut.Parameters.AddWithValue("@no", int.Parse(txtno.Text));
+           // komut.Parameters.AddWithValue("@ad",txtad.Text);
+           // komut.Parameters.AddWithValue("@soyad",txtsoyad.Text);
+           // komut.Parameters.AddWithValue("@tel",txttel.Text);
+            komut.ExecuteNonQuery(); 
+            connection.Close();
+            Listele();
+        }
+
+        private void btngüncelle_Click(object sender, EventArgs e)
+        {
+            komut=new MySqlCommand();
+            connection.Open();
+            komut.Connection = connection;
+            komut.CommandText = "update ogrenci set ad='" + txtad + "',soyad='" + txtsoyad + "',tel='" + txttel.Text + "'where no='" + txtno.Text + "";
+            komut.ExecuteNonQuery();
+            connection.Close();
+            Listele();
+
+        }
+
+        private void btnsil_Click(object sender, EventArgs e)
+        {
+            komut =new MySqlCommand();
+            connection.Open();
+            komut.Connection = connection;
+            komut.CommandText = "delete from ogrenci where no=" + txtno + "";
+            komut.ExecuteNonQuery();
+            connection.Close();
+            Listele();
+
+        }
+
+        private void txtarama_TextChanged(object sender, EventArgs e)
+        {
+            connection=new MySqlConnection("Server=localhost;Database=ögrenciii;Uid=root;Pwd=1234;");
+            adapter=new MySqlDataAdapter("select * from ogrenci where ad like '"+txtarama.Text+"%'", connection);
+            table = new DataTable();
+            connection.Open();
+            adapter.Fill(table);
+            gridadam.DataSource = table;
+            connection.Close();
+        }
+
+        private void gridadam_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
